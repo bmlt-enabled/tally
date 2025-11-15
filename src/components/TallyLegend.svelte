ff66588558147c038a2f3dd1d859c501e051f63c
Patrick Joyce <pjaudiomv@gmail.com>
2025-11-15 10:41:27 -0500

    fix google link

--- src/components/TallyLegend.svelte
+++ src/components/TallyLegend.svelte
@@ -1,7 +1,7 @@
 <div id="tallyLegend">
 	<p class="tallyMo">
 		<sup>1</sup> Indicates if the Google API Key was found to be present. This is necessary for setting coordinates for each meeting entry. More information is available
-		<a href="https://bmlt.app/google-maps-api-keys-and-geolocation-issues/" target="_blank">here</a>.
+		<a href="https://bmlt.app/google-api-key/" target="_blank">here</a>.
 	</p>
 	<p class="tallyMo"><sup>2</sup> Groups counts are estimates. The calculation is done by looking at unique group codes and meeting name matching.</p>
 </div>
